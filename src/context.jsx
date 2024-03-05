2163b54b52397a0a1481bd1b69e41a0ab52cbf34
Vaishu <vaishnavisatrasala1997@gmail.com>
2024-03-05 12:24:48 -0500

    first commit

---
+++ src/context.jsx
@@ -0,0 +1,32 @@
+import { createContext,useState,useContext } from "react";
+ 
+const AppContext = createContext();
+
+export const AppProvider = ({children}) => {
+
+    const [isSideBarOpen,setIsSideBarOpen] = useState(false);
+    const [isModalOpen,setIsModalOpen] = useState(false);
+
+    const openSideBar = () => {
+        setIsSideBarOpen(true);
+    }
+    const closeSideBar = () => {
+        setIsSideBarOpen(false);
+    }
+    const openModal = () => {
+        setIsModalOpen(true);
+    }
+    const closeModal = () => {
+        setIsModalOpen(false);
+    }
+
+    return(
+        <AppContext.Provider value={{isSideBarOpen,isModalOpen,openSideBar,openModal,closeModal,closeSideBar}}>
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
